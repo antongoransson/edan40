7b6dad7b6aa35498ed729ec03899a902d5c72ce1
antongoransson <anton.goransson95@gmail.com>
2018-03-27 12:42:48 +0200

    Add more functions

--- classes/class1/class1.hs
+++ classes/class1/class1.hs
@@ -26,4 +26,31 @@ nextFactor k n
   | n `mod`  (k + 1) == 0 = k + 1
   | otherwise             = nextFactor (k + 1)  n
 
-numFactors n
+numFactors n = length( factorials [1..n])
+   where
+      factorials (x:xs)
+        | x >= n      = n : xs
+        | otherwise   = x : factorials [ nextFactor x n]
+
+
+multiply1 :: Num a => [a] -> a
+
+multiply1 [] = 1
+multiply1 (n:ns) = n * multiply1(ns)
+
+substitute x y string = map(\n -> if n == x then y else n) string
+
+
+duplicates :: Eq a => [a] -> Bool
+duplicates (x:xs)
+  | length xs == 0  = False
+  | elem x xs       = True
+  | otherwise       = duplicates(xs)
+
+removeDuplicates :: Eq a => [a] -> [a]
+removeDuplicates ns = sieve ns []
+   where
+      sieve (n:ns) xs
+        | length ns == 0  = xs
+        | not (elem n xs) = sieve ns (xs ++ [n])
+        | otherwise  = sieve ns xs
